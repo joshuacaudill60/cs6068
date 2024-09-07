424bbb8d4290c2f3702bad9e9d94791145493738
Joshua Caudill <joshuacaudill60@gmail.com>
2024-09-06 20:19:17 -0400

    Add generate_mandelbrot_set.py
    Add sequential Python script for generating the Mandelbrot Set.

---
+++ Assignments/Module 2 Programming Assignment/generate_mandelbrot_set.py
@@ -0,0 +1,56 @@
+"""
+Starter code for the fast generation of the Mandelbrot Set.
+"""
+
+import time
+
+import numpy as np
+from pylab import imshow, show
+
+
+def mandel(x, y, max_iters):
+    """
+    Computes the behavior of '0' under max_iters iterations for the value c.
+    """
+    c = complex(
+        x, y
+    )  # Generate complex number, c, given real and imaginary components.
+    z = 0.0j
+    for i in range(max_iters):
+        z = z * z + c  # Compute the function.
+        if (
+            z.real * z.real + z.imag * z.imag
+        ) >= 4:  # Return iteration value if z becomes larger than 4.
+            return i
+
+    return max_iters  # Return max_iters otherwise.
+
+
+def create_fractal(min_x, max_x, min_y, max_y, img, iters):
+    """
+    The Mandelbrot Set is a fractal. Create the fractal.
+    """
+    height = img.shape[0]
+    width = img.shape[1]
+
+    pixel_size_x = (max_x - min_x) / width
+    pixel_size_y = (max_y - min_y) / height
+
+    for x in range(width):
+        real = min_x + x * pixel_size_x
+        for y in range(height):
+            imag = min_y + y * pixel_size_y
+            color = mandel(real, imag, iters)
+            img[y, x] = color
+
+
+image = np.zeros((1024, 2024), dtype=np.uint8)  # Generate the image.
+
+
+start = time.time()  # Get the start time.
+create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
+end = time.time()  # Get the end time.
+print(f"Elapsed = {(end - start)}")  # Print the elapsed time.
+
+imshow(image)
+show()
